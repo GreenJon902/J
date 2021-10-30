23ba44188a2bfb500e9c20a827e7e29177d60f9b
GreenJon902 <GreenJon902@gmail.com>
2021-10-30 12:35:19 +0100

    more arguement things

--- arguments.py
+++ arguments.py
@@ -31,23 +31,31 @@ def parse(args: list[str]):
     logger.log_dump(f"Parsed args into {args} where path={path}")
 
 
+
     instruction: list[str]
+    # No Instruction  --------------------------------------------------------------------------------------------------
     if len(args) == 0:
         logger.log_error("No instruction given, run \"J.py -h\" if you need help!")
         sys.exit()
 
+
+    # Interpret --------------------------------------------------------------------------------------------------------
     elif args[0] == "interpret":
         if len(args) == 2:
             instruction = ["interpret", args[1]]
-
         elif len(args) == 3:
-            instruction = ["interpret", args[2]]
-
+            if args[1] == "in_file":
+                instruction = ["interpret", args[2]]
+            else:
+                logger.log_error("interpret can only take one positional argument - \"-in  --in_file\", run \"J.py\" "
+                                 "-h if you need help!")
+                sys.exit()
         else:
             logger.log_error("Too many or too little arguments given for instruction \"interpret\", run \"J.py\" -h if "
                              "you need help!")
             sys.exit()
 
+    # Invalid Instruction ----------------------------------------------------------------------------------------------
     else:
         logger.log_error("Invalid instruction, run \"J.py -h\" if you need help!")
         sys.exit()
